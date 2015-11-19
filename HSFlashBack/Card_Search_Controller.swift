aaf7e4e8b1b5b37a38a1cddf1353f077ea20806b
Aakash Shah <ahs2hd@labmini10.local>
2015-11-18 23:40:18 -0500

    API - SwiftyJSON Added

---
+++ HSFlashBack/Card_Search_Controller.swift
@@ -0,0 +1,27 @@
+//
+//  Card_Search_Controller.swift
+//  HSFlashBack
+//
+//  Created by Aakash Shah on 11/18/15.
+//  Copyright © 2015 Matthew King. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import SwiftyJSON
+
+
+class Card_Search: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "background")!)
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+    

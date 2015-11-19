eff401933af88c712ee1cd11ecfddb93ac3d14d9
Aakash Shah <ahs2hd@labmini10.local>
2015-11-19 00:15:04 -0500

    Alamofire is in. Created CardSearch screen

--- HSFlashBack/Card_Search_Controller.swift
+++ HSFlashBack/Card_Search_Controller.swift
@@ -8,15 +8,32 @@
 
 import Foundation
 import UIKit
-import SwiftyJSON
-
+import Alamofire
 
 class Card_Search: UIViewController {
+    
+    var arrRes = [[String:AnyObject]]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor(patternImage: UIImage(named: "background")!)
         // Do any additional setup after loading the view, typically from a nib.
+
+        
+        // Alamofire SwiftyJSON Test
+        /*
+        Alamofire.request(.GET, "http://api.androidhive.info/contacts/").responseJSON { (responseData) -> Void in
+            let swiftyJsonVar = JSON(responseData.result.value!)
+            
+            if let resData = swiftyJsonVar["contacts"].arrayObject {
+                self.arrRes = resData as! [[String:AnyObject]]
+            }
+            if self.arrRes.count > 0 {
+                print(self.arrRes)
+            }
+        }
+        */
+        
     }
     
     override func didReceiveMemoryWarning() {

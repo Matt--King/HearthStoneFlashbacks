004ce36d477f3935ebabb8b60e3f8f225937b5e2
Matt--King <kingmatt333@hotmail.com>
2015-11-21 00:56:39 -0500

    JSON call to Mashape works

--- HSFlashBack/Card_Search_Controller.swift
+++ HSFlashBack/Card_Search_Controller.swift
@@ -14,20 +14,43 @@ class Card_Search: UIViewController {
     
     var arrRes = [[String:AnyObject]]()
     
+    let manager = Alamofire.Manager.sharedInstance
+    
     @IBOutlet weak var CardImageView: UIImageView!
     @IBOutlet weak var SearchButton: UIButton!
     @IBOutlet weak var SearchField: UITextField!
     @IBOutlet weak var ArtistLabel: UILabel!
     @IBOutlet weak var FlavorLabel: UILabel!
+
+    let domain = "https://omgvamp-hearthstone-v1.p.mashape.com/cards/"
+    let params = "?collectible=1&locale=enUS"
+    var img:UIImage? = nil
+    var imgGold:UIImage? = nil
+    var artist:AnyObject? = nil
+    var flavor:AnyObject? = nil
+    var imgURL:AnyObject? = nil
+    var imgGoldURL:AnyObject? = nil
     
-    let mashape = "\"cardId\":\"EX1_572\",\"name\":\"Ysera\",\"cardSet\":\"Classic\",\"type\":\"Minion\",\"faction\":\"Neutral\",\"rarity\":\"Legendary\",\"cost\":9,\"attack\":4,\"health\":12,\"text\":\"At the end of your turn, add a Dream Card to your hand.\",\"flavor\":\"Ysera rules the Emerald Dream.  Which is some kind of green-mirror-version of the real world, or something?\",\"artist\":\"Gabor Szikszai\",\"collectible\":true,\"elite\":true,\"race\":\"Dragon\",\"img\":\"http://wow.zamimg.com/images/hearthstone/cards/enus/original/EX1_572.png\",\"imgGold\":\"http://wow.zamimg.com/images/hearthstone/cards/enus/animated/EX1_572_premium.gif\",\"locale\":\"enUS\"}"    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor(patternImage: UIImage(named: "background")!)
         // Do any additional setup after loading the view, typically from a nib.
 
+        // Setting Headers for API session
+        //manager.session.configuration.HTTPAdditionalHeaders =
+        let headers = ["X-Mashape-Key": "CRoLBfeR9UmshwZTcCyMTcwohJv6p1kZPEWjsnws6uslpg1S7S"
+            ,"Accept": "application/json"]
         
+        manager.request(.GET,domain+"ysera"+params, headers:headers).responseJSON {
+            (responseData) -> Void in
+            let swiftyResp = JSON(responseData.result.value!)
+            print(swiftyResp)
+            //artist = swiftyResp[0]["artist"].dictionaryValue
+            //flavor = swiftyResp[0]["flavor"].dictionaryValue
+
+        }
+
         // Alamofire SwiftyJSON Test
         
         Alamofire.request(.GET, "http://api.androidhive.info/contacts/").responseJSON { (responseData) -> Void in
@@ -40,6 +63,7 @@ class Card_Search: UIViewController {
                 print(self.arrRes)
             }
         }
+
 
         
     }
